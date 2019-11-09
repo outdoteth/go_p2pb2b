0b7698bc51cae6c416242c75763dbd54742021de
Dylan-Phoon <dylanphoon@gmail.com>
2019-11-09 18:22:36 -0500

    add ioutil to read output and http.MethodGet check

--- go_p2pb2b.go
+++ go_p2pb2b.go
@@ -3,6 +3,7 @@ package go_p2pb2b
 import (
 	"context"
 	"net/http"
+	"io/ioutil"
 )
 
 //Client object for initial parameters
@@ -28,8 +29,11 @@ func New_Client(url, api_key string, ctx context.Context) *Client {
 
 func (clt *Client) API_request(method, endpoint string) (*http.Response, error) {
 	ctx := clt.Ctx
-
-	req, err := http.NewRequest(method, clt.URL + endpoint, nil)
+	var req *http.Request
+	var err error
+	if method == http.MethodGet {
+		req, err = http.NewRequest(method, clt.URL + endpoint, nil)
+	}
 
 	if method == http.MethodPost {
 		//run through all of the signing procedures
@@ -45,16 +49,24 @@ func (clt *Client) API_request(method, endpoint string) (*http.Response, error) 
 	return res, nil
 }
 
+
+
 //this is where we need a custom response type so its easy for the end user
-func (clt *Client) get_markets() (*http.Response, error) {
+func (clt *Client) get_markets() (string, error) {
 	endpoint := "/public/markets"
 	res, err := clt.API_request(http.MethodGet, endpoint)
 
 	if err != nil {
-		return res, err
+		return "", err
 	}
 
-	return res, nil
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
 }
 
 
